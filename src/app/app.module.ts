672782bdb67302da1081a42e3f19d61b88269d80
Georgios <1722374@stud.hs-mannheim.de>
2020-07-07 04:12:24 +0200

    Erster commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ProfessorModule} from './professor/professor.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProfessorModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
